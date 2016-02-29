7842e92ebd1ef9f542760f6a563a3f5a25ef9d00
Eric Schultz <eschultz@prplfoundation.org>
2016-02-29 13:48:03 -0600

    correct documentation in connection_decider.py
    Signed-off-by: Eric Schultz <eschultz@prplfoundation.org>

--- devices/connection_decider.py
+++ devices/connection_decider.py
@@ -3,16 +3,13 @@ import local_serial_connection
 
 def connection(conn_type, device, **kwargs):
     '''
-    Depending on the given model, return an object of the appropriate class type.
-
-    Different boards are flashed with different commands, have
-    different memory addresses, and must be handled differently.
+    Depending on the given connection type return an object of the appropriate class type.
     '''
     if conn_type in ("ser2net"):
         return ser2net_connection.Ser2NetConnection(device=device,**kwargs)
 
     if conn_type in ("local_serial"):
-        return local_serial_connection.LocalSerialConnection(device=device,**kwargs)
+        return local_serial_connection.LocalSerialConnection(device=device**kwargs)
 
     # Default for all other models
     print("\nWARNING: Unknown connection type  '%s'." % type)
