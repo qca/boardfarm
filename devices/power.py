97ad4cd7add1ffc2220e64df110df20f87466688
Eric Schultz <eschultz@prplfoundation.org>
2016-03-23 20:53:41 -0500

    add missing arguments
    Signed-off-by: Eric Schultz <eschultz@prplfoundation.org>

--- devices/power.py
+++ devices/power.py
@@ -8,7 +8,8 @@
 import pexpect
 import dlipower
 
-def get_power_device(ip_address, outlet=None):
+
+def get_power_device(ip_address, username=None, password=None, outlet=None):
     '''
     Try to determine the type of network-controlled power switch
     at a given IP address. Return a class that can correctly
@@ -30,9 +31,9 @@ def get_power_device(ip_address, outlet=None):
         raise Exception("\nError connecting to %s" % ip_address)
     if t == 0:
         return DLIPowerSwitch(ip_address, outlet=outlet, username=username, password=password)
-    if t == 1:
+    elif t == 1:
         return SentrySwitchedCDU(ip_address, outlet=outlet)
-    if t == 2:
+    elif t == 2:
         return APCPower(ip_address, outlet=outlet)
     else:
         raise Exception("No code written to handle power device found at %s" % ip_address)
