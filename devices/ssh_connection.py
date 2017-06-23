a4bea3ec5439e596c2c874e29ec0bb588a3e3412
Bartlomiej Zielinski <b.zielinski@radytek.com>
2017-06-23 11:11:56 +0200

    Add support for SSH connection.
    It is possible to connect to the tested board with ssh. Authentication
    can be done with password, ssh key without password or ssh password
    protected key. To use ssh connection you need json config file properties:
    'connection_type': 'ssh',
    'ssh_password': <password>,

--- devices/ssh_connection.py
+++ devices/ssh_connection.py
@@ -3,14 +3,14 @@ import pexpect
 
 class SshConnection:
     '''
-        To use, set conn_cmd in your json to "ssh root@192.168.1.1 -i ~/.ssh/id_router_key""
+        To use, set conn_cmd in your json to "ssh root@192.168.1.1 -i ~/.ssh/id_rsa""
         and set connection_type to "ssh"
 
         '''
-    def __init__(self, device=None, conn_cmd=None, key_password='None', **kwargs):
+    def __init__(self, device=None, conn_cmd=None, ssh_password='None', **kwargs):
         self.device = device
         self.conn_cmd = conn_cmd
-        self.key_password = key_password
+        self.ssh_password = ssh_password
 
     def connect(self):
         pexpect.spawn.__init__(self.device,
@@ -18,13 +18,13 @@ class SshConnection:
                                args=['-c', self.conn_cmd])
 
         try:
-            result = self.device.expect(["assword:", "passphrase"])
+            result = self.device.expect(["assword:", "passphrase"] + self.device.prompt)
         except pexpect.EOF as e:
             raise Exception("Board is in use (connection refused).")
         if result == 0 or result == 1:
-            self.device.sendline(self.key_password)
-            prompt = ['root\\@.*:.*#', '/ # ', '@R7500:/# ']
-            self.device.expect(prompt)
+            assert self.ssh_password is not None, "Please add ssh_password in your json configuration file."
+            self.device.sendline(self.ssh_password)
+            self.device.expect(self.device.prompt)
 
     def close(self):
         self.device.sendline('exit')
